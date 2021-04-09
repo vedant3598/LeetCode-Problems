5a1e4c538f3c825a6dd37bdd29873b7e3c6f1fa7
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-04-09 02:50:44 -0400

    Solution for Happy Number

---
+++ src/happynumber.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int temp = 0;
+        vector<int> numberSeen;
+        while(n != 1){
+            temp = n;
+            vector<int> num;
+            while(temp != 0){
+                num.push_back(temp % 10);
+                temp /= 10;
+            }
+            temp = 0;
+            for(int i = 0; i < num.size(); i++){
+                int sqr = num[i] * num[i];
+                temp += sqr;
+            }    
+            n = temp;
+            for(int i = 0; i < numberSeen.size(); i++){
+                if(numberSeen[i] == n)
+                    return false;
+            }
+            numberSeen.push_back(n);
+        }
+        return true;
+    }
+};
