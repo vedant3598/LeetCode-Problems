49ea0f4460c88adcc9f3ad11364eb4b728f9ecba
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-04-09 02:51:23 -0400

    Leetcode Solutions

---
+++ src/valid-parentheses.cpp
@@ -0,0 +1,37 @@
+#include <stack>
+using namespace std;
+class Solution {
+public:
+    bool isValid(string s) {
+        int p = 0; 
+        stack<char> paren;
+        for(int i = 0; i < s.size(); i++){
+            char t = s[i];
+            if(t == '{' || t == '[' || t == '('){
+                paren.push(s[i]);
+                p++;
+            }
+            else{
+                if(t == ')' && !paren.empty() && paren.top() == '('){
+                    paren.pop();
+                    p--;
+                }
+                else if(t == ']' && !paren.empty() && paren.top() == '['){
+                    paren.pop();
+                    p--;
+                }
+                else if(t == '}' && !paren.empty() && paren.top() == '{'){
+                    paren.pop();
+                    p--;
+                }
+                else
+                    return false;
+            }
+        }
+        if(p != 0)
+            return false;
+        else
+            return true;
+        
+    }
+};
