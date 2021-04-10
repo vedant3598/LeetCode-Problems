5c3d63507546cf2175b1ddb5f06bc8b671e226e5
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-04-10 01:25:10 -0400

    LeetCode Solutions

---
+++ src/deletenodeinabst.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root){
+        if(root->left == nullptr){
+        }
+        else{
+            root = inorderSuccessor(root->left);
+        }
+        return root;
+    }
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        
+        if(root == nullptr){
+            return root;
+        }
+        
+        if(root->val > key){
+            root->left = deleteNode(root->left, key);
+        }
+        else if(root->val < key){
+            root->right = deleteNode(root->right, key);
+        }
+        else{
+            if(root->left == nullptr && root->right == nullptr){
+                return nullptr;
+            }
+            else if(root->left == nullptr){
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            }
+            else if(root->right == nullptr){
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+
+            else{
+                TreeNode* temp = inorderSuccessor(root->right);
+                root->val = temp->val;
+                root->right = deleteNode(root->right, temp->val);
+            }
+        }
+        return root;   
+    }
+};
