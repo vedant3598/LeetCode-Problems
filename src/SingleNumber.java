f807493780007ff89a88b6252fd87787a53464bc
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-04-11 16:40:47 -0400

    LeetCode Solutions

---
+++ src/SingleNumber.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int returnNum = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            if(i == nums.length - 1){
+                returnNum = nums[i];
+                break;
+            }
+            
+            if(i % 2 == 0){
+                if(nums[i] != nums[i + 1]){
+                    returnNum = nums[i];
+                    break;
+                }
+            }
+        
+        }
+        return returnNum;
+    }
+}
