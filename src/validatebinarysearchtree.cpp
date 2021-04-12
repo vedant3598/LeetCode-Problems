ca99b9c0fc80753a8e31bae9623d4c43274a1579
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-04-11 23:13:51 -0400

    LeetCode Solutions

---
+++ src/validatebinarysearchtree.cpp
@@ -0,0 +1,32 @@
+using namespace std;
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        if(root->left == nullptr && root->right == nullptr)
+            return true;
+        long long num = 1e18;
+        return isValid(root, -num, num);
+    }
+    
+    bool isValid(TreeNode* root, long long low, long long high){    
+        if(root == nullptr){
+            return true;
+        }
+        else{
+            if(!(root->val > low && root->val < high))
+                return false;
+        }
+        return isValid(root->left, low, root->val) && isValid(root->right, root->val, high);
+    }
+};
