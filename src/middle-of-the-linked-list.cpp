b56d21a5597ab9a0a9a283fdd272108ece0aa079
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-04-25 17:23:18 -0400

    LeetCode Solutions

---
+++ src/middle-of-the-linked-list.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* ret = nullptr;
+        
+        while(true){
+            if(fast->next == nullptr){
+                // odd
+                ret = slow;
+                break;
+            }
+            else if(fast->next->next == nullptr){
+                // even
+                ret = slow->next;
+                break;
+            }
+            else{
+                slow = slow->next;
+                fast = fast->next->next;
+            }
+        }
+        return ret;
+    }
+};
