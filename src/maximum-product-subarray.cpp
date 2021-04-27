39c14ed3360b80171c2d9dade8208e21e1100888
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-04-26 22:37:35 -0400

    LeetCode Solutions

---
+++ src/maximum-product-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int max = INT_MIN;
+        int current = 0;
+        for(int i = 0; i < nums.size(); i++){
+            current = nums[i];
+            for(int x = i; x < nums.size(); x++){
+                if(i != x)
+                    current *= nums[x];
+                
+                if(current > max)
+                    max = current;
+            }
+        }
+        return max;
+    }
+};
