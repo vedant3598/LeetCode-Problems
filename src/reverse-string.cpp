8aa4ec3f04b8a3ea6ec2026163606b3efca83a94
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-04-29 21:41:58 -0400

    LeetCode Solutions

---
+++ src/reverse-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        char* first = &s[0];
+        char* last = &s[s.size() - 1];
+        
+        while(first <= last){
+            char temp = *first;
+            *first = *last;
+            *last = temp;
+            first++;
+            last--;
+        }
+    }
+};
