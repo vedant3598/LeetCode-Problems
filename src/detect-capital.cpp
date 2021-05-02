098eee6c27064b0fb34bf2c6f98c9fc3a45366bb
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-05-01 20:40:39 -0400

    LeetCode Solutions

---
+++ src/detect-capital.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int counter = 0;
+        if(isupper(word[counter])){
+            counter++;
+            if(word.size() == 1)
+                return true;
+            else{
+                if(isupper(word[counter])){
+                    counter++;
+                    while(counter != word.length()){
+                        if(!isupper(word[counter]))
+                            return false;
+                        counter++;
+                    }
+                }
+                else{
+                    counter++;
+                    while(counter != word.length()){
+                        if(isupper(word[counter]))
+                            return false;
+                        counter++;
+                    }
+                }
+            }
+        }
+        else{
+            while(counter != word.length()){
+                if(isupper(word[counter]))
+                    return false;
+                counter++;
+            }
+        }
+        return true;
+    }
+};
