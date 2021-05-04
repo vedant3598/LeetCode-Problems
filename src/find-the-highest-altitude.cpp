973a556e3922d64c9024b3f3a90b1d348cc4fef5
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-05-03 22:58:51 -0400

    LeetCode Solutions

---
+++ src/find-the-highest-altitude.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int highestAltitude = 0;
+        int current = 0;
+        
+        for(int i = 0; i < gain.size(); i++){
+            current += gain[i];
+            if(current > highestAltitude)
+                highestAltitude = current;
+        }
+        
+        return highestAltitude;
+    }
+};
