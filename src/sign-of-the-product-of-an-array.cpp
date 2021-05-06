0715d011f5e1bcf5b6527d104d1ce9e1a5b7850c
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-05-05 22:17:15 -0400

    LeetCode Solutions

---
+++ src/sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int product = 1;
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == 0)
+                product *= 0;
+            else if(nums[i] > 0)
+                product *= 1;
+            else
+                product *= -1;
+        }
+        
+        if(product > 0)
+            return 1;
+        else if(product < 0)
+            return -1;
+        else
+            return 0;
+    }
+};
