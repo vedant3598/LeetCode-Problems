7ab0f5717472e4e8e4230ac3888b5c3c9a5b6f48
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-05-08 20:30:36 -0400

    LeetCode Solutions

---
+++ src/sum-of-unique-elements.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        int sum = 0;
+        map<int, int> dict;
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(dict.find(nums[i]) == dict.end()){
+                dict[nums[i]] = 0;
+                sum += nums[i];
+            }
+            else{
+                if(dict[nums[i]] == 0){
+                    sum -= nums[i];
+                    dict[nums[i]]++;
+                }
+            }
+        }
+
+        return sum;
+    }
+};
