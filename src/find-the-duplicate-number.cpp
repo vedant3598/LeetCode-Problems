a7c61db3a90a820268624e894f145f28ff5b43f1
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-05-11 23:25:26 -0400

    Working Solution to find-the-duplicate-number

---
+++ src/find-the-duplicate-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int duplicate;
+        for(int i = 0; i < nums.size(); i++){
+            int num = nums[i];
+            int index = num - 1;
+            if(i != index){
+                int temp = nums[index];
+                if(temp == num){
+                    duplicate = temp;
+                    break;   
+                }
+                nums[index] = num;
+                nums[i] = temp;
+            }
+        }
+        return duplicate;        
+    }
+};
