40ed566483de0c971839c1a0858ba55fa9f5c336
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-05-16 02:08:36 -0400

    LeetCode Solutions

---
+++ src/pascals-triangle.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> pascal;
+    
+    void generateUtil(vector<int> v, int &numRows) {
+        if(numRows == 0)
+            return;
+        
+        if(v.size() == 1){
+            vector<int> second;
+            second.push_back(1);
+            second.push_back(1);
+            pascal.push_back(second);
+            generateUtil(second, --numRows);
+        }
+        else{
+            vector<int> second;
+            second.push_back(1);
+            int num1 = 0;
+            int num2 = 1;
+            while(num1 != v.size() - 1){
+                int num = v[num1] + v[num2];
+                second.push_back(num);
+                num1++;
+                num2++;
+            }
+            second.push_back(1);
+            pascal.push_back(second);
+            generateUtil(second, --numRows);
+        }
+    }
+    
+    vector<vector<int>> generate(int numRows) {
+        vector<int> first;
+        first.push_back(1);
+        pascal.push_back(first);
+        numRows--;
+        generateUtil(first, numRows);
+        return pascal;
+    }
+};
