096c62605cd407e1a435dd46eea09648ba7ea0f7
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-05-18 10:30:59 -0400

    LeetCode Solutions

---
+++ src/remove-element.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int counter = 0;
+        if(nums.size() == 0)
+            return counter;
+        
+        int* p1 = &nums[0];
+        int* p2 = &nums[nums.size() - 1];
+        
+        while(p1 <= &nums[nums.size() - 1] && p2 >= &nums[0]){
+            if(*p1 != val){
+                counter++;
+            }
+            else{
+                while(*p2 == val && p2 != p1){
+                    p2--;
+                }
+                if(p2 == p1)
+                    break;
+                else{
+                    *p1 = *p2;
+                    *p2 = val;
+                    counter++;
+                }
+            }
+            p1++;
+        }
+        
+        return counter;
+    }
+};
