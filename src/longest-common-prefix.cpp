5592cb25eecd58f1b47a48a3a5d3211f88598060
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-05-20 21:16:31 -0400

    LeetCode Solutions

---
+++ src/longest-common-prefix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix = strs[0];
+        
+        for(int i = 1; i < strs.size(); i++){
+            string temp = "";
+            int counter = 0;
+            while(counter < prefix.length() && counter < strs[i].length()){
+                if(prefix[counter] == strs[i][counter]){
+                    temp += prefix[counter];
+                    counter++;
+                }
+                else
+                    break;
+            }
+            if(temp == "")
+                return "";
+            else
+                prefix = temp;
+        }
+        
+        return prefix;
+        
+    }
+};
