963d1fa5b76d516e326dc7daf8ae772c323d0cb1
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-05-22 00:10:28 -0400

    LeetCode Solutions

--- src/odd-even-linked-list.cpp
+++ src/odd-even-linked-list.cpp
@@ -11,13 +11,43 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        ListNode* newHead = nullptr;
+        if(head == nullptr)
+            return head;
+        
+        ListNode* newHeadOdd = nullptr;
+        ListNode* newHeadEven = nullptr;
+        int counter = 1;
+        ListNode* odd = nullptr;
+        ListNode* even = nullptr;
         
         while(head != nullptr){
-            
+            // odd
+            if(counter % 2 != 0){
+                if(odd == nullptr){
+                    odd = new ListNode(head->val);
+                    newHeadOdd = odd;
+                }
+                else{
+                    odd->next = new ListNode(head->val);
+                    odd = odd->next;
+                }
+            }
+            else{
+                if(even == nullptr){
+                    even = new ListNode(head->val);
+                    newHeadEven = even;
+                }
+                else{
+                    even->next = new ListNode(head->val);
+                    even = even->next;
+                }
+            }
+            head = head->next;
+            counter++;
         }
-        
-        return newHead;
+        odd->next = newHeadEven;
+
+        return newHeadOdd;
         
         
     }
