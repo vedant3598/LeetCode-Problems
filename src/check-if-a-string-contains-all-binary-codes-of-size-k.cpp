0b61e583f4f7e41c6d9c8aa0d59160f62c85bb32
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-05-23 02:08:47 -0400

    LeetCode Solutions

--- src/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ src/check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     bool hasAllCodes(string s, int k) {
         int codes = pow(2, k);
-        const int power = 2;
         map<int, int> dict;
         int i = 0;
         int counter = 0;
         
-
         while(counter < s.length()){
             int sum = 0;
             int current = k;
             i = counter;
-            while(current != 0){
+            while(current != 0 && i < s.length()){
                 if(s[i] == '1'){
                     sum += pow(2, current-1);
                 }
                 current--;
                 i++;
             }
-            if(dict.find(sum) == dict.end()){
+            if(current == 0 && dict.find(sum) == dict.end()){
                 dict[sum] == 0;
                 if(dict.size() == codes)
                     return true;
