ac60305c8f39d0dc273470b6d5dc22795e9cd137
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-05-23 21:08:33 -0400

    LeetCode Solutions

---
+++ src/length-of-last-word.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length = 0;
+        vector<int> all;
+        string word = "";
+        
+        for(int i = 0; i < s.length(); i++){
+            while(i < s.length() && s[i] != ' '){
+                word += s[i];
+                i++;
+            }
+            if(word.length() > 0)
+                all.push_back(word.length());
+            word = "";
+        }
+        
+        if(all.size() > 0)
+            return all[all.size() - 1];
+        else
+            return 0;
+    }
+};
