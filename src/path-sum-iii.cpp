b7f30b81f9197cb467a7b29992b7c3f695a24697
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-06-04 16:28:06 -0400

    LeetCode Solutions

---
+++ src/path-sum-iii.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int count = 0;
+    
+    void pathSumUtil(TreeNode* root, const int &targetSum, vector<int> &curr){
+        if(root == nullptr)
+            return;
+        
+        cout << root->val << endl;
+        
+        for(int i = 0; i < curr.size(); i++){
+            curr[i] += root->val;
+            if(curr[i] == targetSum){
+                count++;
+            }
+        }
+        curr.push_back(root->val);
+        if(curr[curr.size() - 1] == targetSum)
+            count++;
+        
+        vector<int> right = curr;
+        pathSumUtil(root->left, targetSum, curr);
+        pathSumUtil(root->right, targetSum, right);
+        
+        return;
+    }
+    
+    
+    int pathSum(TreeNode* root, int targetSum) {
+        vector<int> curr;
+        pathSumUtil(root, targetSum, curr);
+        return count;
+    }
+};
