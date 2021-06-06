70ac821abc96b57975831ed87fb5898b16f5583e
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-06-06 01:55:01 -0400

    LeetCode Solutions

---
+++ src/search-in-a-binary-search-tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* ans = nullptr;
+    
+    void searchBSTUtil(TreeNode* root, const int &val){
+        if(root == nullptr)
+            return;
+        
+        if(root->val == val)
+            ans = root;
+        
+        searchBSTUtil(root->left, val);
+        searchBSTUtil(root->right, val);
+        
+        return;
+    }
+    
+    TreeNode* searchBST(TreeNode* root, int val) {
+        searchBSTUtil(root, val);
+        return ans;
+    }
+};
