1bdef1b8b5d5dafec73cadb5a6215b770a067b13
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-06-06 00:20:53 -0400

    LeetCode Solutions

--- src/reverse-vowels-of-a-string.cpp
+++ src/reverse-vowels-of-a-string.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     bool isVowel(char c){
-        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
     }
     
     string reverseVowels(string s) {
@@ -12,16 +13,34 @@ public:
         char* p2 = &s[s.length() - 1];
         
         while(p1 <= p2){
-            if(isVowel(*p1)){
-                while(!isVowel(*p2)){
-                    p2--;
-                }
+            if(isVowel(*p1) && isVowel(*p2)){
                 char temp = *p2;
                 *p2 = *p1;
                 *p1 = temp;
             }
-            else
-                p1++;
+            else if(isVowel(*p1)){
+                while(p1 <= p2 && !isVowel(*p2)){
+                    p2--;
+                }
+                if(p1 > p2)
+                    break;
+                char temp = *p2;
+                *p2 = *p1;
+                *p1 = temp;
+            }
+            else if(isVowel(*p2)){
+                while(p1 <= p2 && !isVowel(*p1)){
+                    p1++;
+                }
+                if(p1 > p2)
+                    break;
+                char temp = *p2;
+                *p2 = *p1;
+                *p1 = temp;
+            }
+            else {}
+            p1++;
+            p2--;
         }
         return s;
     }
