4b00ac9d7450d80f336b83489145d977884da59c
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-06-07 01:14:25 -0400

    LeetCode Solutions

--- src/symmetric-tree.cpp
+++ src/symmetric-tree.cpp
@@ -10,18 +10,59 @@
  * };
  */
 class Solution {
-public:  
-    bool isSymmetricUtil(vector<TreeNode*> parent){
+public:
+    
+    bool isSymmetricUtil(vector<TreeNode*> &parent){
+        bool symmetric = true;
+        
+        int val = 0;
+        for(int i = 0; i < parent.size(); i++){
+            if(parent[i] == nullptr)
+                val++;
+        }
+        if(val == parent.size())
+            return symmetric;
+        
         vector<TreeNode*> children;
         for(int i = 0; i < parent.size(); i++){
-            children.push_back(parent[i]->left);
-            children.push_back(parent[i]->right);
+            if(parent[i] == nullptr)
+                children.push_back(nullptr);
+            else{
+                children.push_back(parent[i]->left);
+                children.push_back(parent[i]->right);
+            }
         }
+        
+        if(children.size() % 2 == 0){
+            int p1 = 0;
+            int p2 = children.size() - 1;
+            while(p1 <= p2){
+                if(children[p1] != nullptr && children[p2] != nullptr){
+                    if(children[p1]->val != children[p2]->val){
+                        symmetric = false;
+                        break;
+                    }
+                }
+                else{
+                    if(!(children[p1] == nullptr && children[p2] == nullptr)){
+                        symmetric = false;
+                        break;
+                    }
+                }
+                p1++;
+                p2--;
+            }
+        }
+        else
+            return false;
+        
+        return symmetric && isSymmetricUtil(children);
     }
+    
     
     bool isSymmetric(TreeNode* root) {
         vector<TreeNode*> parent;
         parent.push_back(root);
-        return isSymmemtricUtil(parent);
+        return isSymmetricUtil(parent);
     }
 };
