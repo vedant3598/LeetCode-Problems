e78803346a1ccb9c0dd49225ce3ddbae057d35b5
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-06-08 18:59:04 -0400

    LeetCode Solutions

---
+++ src/reverse-words-in-a-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stack<string> words;
+        string reverse = "";
+        
+        for(int i = 0; i < s.length(); i++){
+            string temp = "";
+            while(i != s.length() && s[i] != ' '){
+                temp += s[i];
+                i++;
+            }
+            if(temp != "")
+                words.push(temp);
+        }
+        
+        while(!words.empty()){
+            reverse += words.top();
+            words.pop();
+            if(!words.empty())
+                reverse += ' ';
+        }
+        return reverse;
+        
+    }
+};
