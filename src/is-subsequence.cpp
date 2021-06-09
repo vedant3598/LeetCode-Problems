1058f622c76fc58e3ff93c706c5360acec092e32
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-06-09 10:24:18 -0400

    LeetCode Solutions

--- src/is-subsequence.cpp
+++ src/is-subsequence.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        if(s == "")
-            return true;
-        
         int counter = 0;
-        string construct = "";
         for(int i = 0; i < t.length(); i++){
-            while(i < t.length() && t[i] != s[counter]){
-                i++;
-            }
-            construct += t[i];
-            counter++;
+            if(counter < s.length() && t[i] == s[counter])
+                counter++;
         }
         
-        if(construct == s )
+        if(counter == s.length())
             return true;
         return false;
     }
