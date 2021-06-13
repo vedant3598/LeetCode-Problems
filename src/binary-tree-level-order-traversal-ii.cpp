eaa1d824ff6e0b81e884c06ae5367cc2d2e09261
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-06-13 00:52:59 -0400

    LeetCode Solutions

--- src/binary-tree-level-order-traversal-ii.cpp
+++ src/binary-tree-level-order-traversal-ii.cpp
@@ -11,7 +11,33 @@
  */
 class Solution {
 public:
-    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+    vector<vector<int>> nodes;
+    
+    void levelOrderBottomUtil(vector<TreeNode*> parent){
+        if(parent.size() == 0)
+            return;
         
+        vector<int> nums;
+        vector<TreeNode*> children;
+        for(int i = 0; i < parent.size(); i++){
+            nums.push_back(parent[i]->val);
+            if(parent[i]->left != nullptr)
+                children.push_back(parent[i]->left);
+            if(parent[i]->right != nullptr)
+                children.push_back(parent[i]->right);
+        }
+        levelOrderBottomUtil(children);
+        
+        nodes.push_back(nums);
+        return;
+    }
+    
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        if(root == nullptr)
+            return nodes;
+        vector<TreeNode*> parent;
+        parent.push_back(root);
+        levelOrderBottomUtil(parent);
+        return nodes;
     }
 };
