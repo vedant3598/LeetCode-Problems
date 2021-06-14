ad2c6fa058329836d95d91ed908ae8729b99e303
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-06-14 19:52:24 -0400

    LeetCode Solutions

---
+++ src/first-unique-character-in-a-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        map<int, int> dict;
+        
+        for(int i = 0; i < s.length(); i++){
+            dict[s[i]]++;
+        }
+        
+        int index = -1;
+        int counter = 0;
+        
+        while(counter < s.length() && index == -1){
+            if(dict[s[counter]] == 1){
+                index = counter;
+                break;
+            }
+            counter++;
+        }
+        
+        return index;
+    }
+};
