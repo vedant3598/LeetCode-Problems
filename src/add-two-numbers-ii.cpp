b8f408658e7671672e7fc06dc834a4469287c56c
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-06-15 21:02:21 -0400

    LeetCode Solutions

--- src/add-two-numbers-ii.cpp
+++ src/add-two-numbers-ii.cpp
@@ -9,56 +9,73 @@
  * };
  */
 class Solution {
-public:
+public:   
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int l1_length = 0;
-        int l2_length = 0;
-        
-        bool l1_larger = false;
-        
-        ListNode* temp = l1;
-        while(temp != nullptr){
-            l1_length++;
-            temp = temp->next;
+        // Lines 15-26: Converting Linked List into string
+        string l1_s = "";
+        string l2_s = "";
+                
+        while(l1 != nullptr){
+            l1_s += l1->val;
+            l1 = l1->next;
         }
         
-        temp = l2;
-        while(temp != nullptr){
-            l2_length++;
-            temp = temp->next;
+        while(l2 != nullptr){
+            l2_s += l2->val;
+            l2 = l2->next;
         }
         
-        ListNode* head = nullptr;
+        // Lines 29-71: Starting from the end of the strings, adding the numbers from the strings and putting them in front of the return Linked List
         ListNode* ret = nullptr;
-        ListNode* prev = nullptr;
-        if(l2_length == l1_length){
-            while(l1 != nullptr){
-                if(ret == nullptr){
-                    ret = new ListNode(l1->val + l2->val);
-                    prev = ret;
+        bool carryOver = false;
+        int l1_length = l1_s.length() - 1;
+        int l2_length = l2_s.length() - 1;
+        int sum = 0;
+        
+        while(l1_length >= 0 || l2_length >= 0){
+            if(ret == nullptr){
+                sum = int(l1_s[l1_length]) + int(l2_s[l2_length]);
+
+                if(sum > 9){
+                    carryOver = true;  
+                    ret = new ListNode(sum - 10);
                 }
-                else{
-                    int sum = l1->val + l2->val;
-                    int num = -1;
-                    if(sum > 9)
-                        num = sum - 10;
-                    
-                    if(num >= 0)
-                        prev->val += 1;
-                    else
-                        num = sum;
-                    
-                    prev->next = new ListNode(num);
-                    prev = prev->next;
-                }
-                l1 = l1->next;
-                l2 = l2->next;
+                else
+                    ret = new ListNode(sum);
             }
+            else{
+                if(l1_length >= 0 && l2_length >= 0)
+                    sum = int(l1_s[l1_length]) + int(l2_s[l2_length]);    
+                else if(l1_length >= 0)
+                    sum = int(l1_s[l1_length]);
+                else if(l2_length >= 0)
+                    sum = int(l2_s[l2_length]);
+
+                if(carryOver)
+                    sum += 1;
+
+                if(sum > 9){
+                    carryOver = true;
+                    sum -= 10;
+                }
+                else
+                    carryOver = false;
+
+                ListNode* temp = new ListNode(sum);
+                temp->next = ret;
+                ret = temp;
+            }   
+            sum = 0;
+            l1_length--;
+            l2_length--;
         }
-        return ret;
-            
-        
-        
-        
+        // Lines 73-77: If carryover remains from above, then add that to the front of the Linked List
+        if(carryOver){
+            ListNode* temp = new ListNode(1);
+            temp->next = ret;
+            ret = temp;
+        }
+                
+        return ret;        
     }
 };
