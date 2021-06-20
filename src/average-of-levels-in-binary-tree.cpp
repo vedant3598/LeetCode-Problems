ac8e2b74351c3205a39f6918a80e1d0863136a26
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-06-20 14:08:52 -0400

    LeetCode Solutions

---
+++ src/average-of-levels-in-binary-tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> average;
+    
+    void averageOfLevelsUtil(vector<TreeNode*> parent){
+        if(!parent.size())
+            return;
+        
+        vector<TreeNode*> children;
+        double sum  = 0;
+        for(int i = 0; i < parent.size(); i++){
+            sum += parent[i]->val;
+            if(parent[i]->left)
+                children.push_back(parent[i]->left);
+            if(parent[i]->right)
+                children.push_back(parent[i]->right);
+        }
+        average.push_back(sum/parent.size());
+        averageOfLevelsUtil(children);
+        return;
+    }
+    
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<TreeNode*> parent;
+        parent.push_back(root);
+        averageOfLevelsUtil(parent);
+        return average;
+    }
+};
