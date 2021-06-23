9b2378d68195564f11d4300de1f3b6bc4eb5b3f7
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-06-23 15:43:13 -0400

    LeetCode Solutions

---
+++ src/house-robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector<int> memo;
+        // In DP problems, you want to have two elements in a dp or memo vector (or array) before recursing or iterating
+        memo.push_back(0);
+        memo.push_back(nums[0]);
+        
+        for(int i = 1; i < nums.size(); i++){
+            memo.push_back(max(memo[i], memo[i-1] + nums[i]));
+        }
+        
+        return memo[nums.size()];
+    }
+};
