03418564fd41fbd8bd9b7c54033f2278b313cfb2
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-06-26 15:49:54 -0400

    PerfectNumber Solution

--- src/PerfectNumber.java
+++ src/PerfectNumber.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class PerfectNumber {
-	public boolean checkPerfectNumber(int num) {
+	public boolean checkPerfectNumber(int num){
         if(num == 0)
             return false;
         ArrayList<Integer> perfectNum = new ArrayList<Integer>();
