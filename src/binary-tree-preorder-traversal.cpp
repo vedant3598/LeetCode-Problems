b20c1bed1109c576d95c3a845a5c1b82b68dd038
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-07-05 13:27:16 -0400

    binary-tree-preorder-traversal solution

--- src/binary-tree-preorder-traversal.cpp
+++ src/binary-tree-preorder-traversal.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     vector<int> preorderTraversalVector(TreeNode* root, vector<int> nodes){
