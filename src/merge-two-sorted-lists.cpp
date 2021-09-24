ce4784b3982228fe5b37cef25b22730fadfe70d7
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2021-09-23 22:16:16 -0400

    merge-two-sorted-lists solution

--- src/merge-two-sorted-lists.cpp
+++ src/merge-two-sorted-lists.cpp
@@ -9,78 +9,73 @@
  * };
  */
 class Solution {
-public:    
-    ListNode* head = nullptr;
-    ListNode* lastNode(ListNode* current){
-        while(current->next != nullptr){
-            current = current->next;
-        }
-        return current;
-    }   
-    
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) { 
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         if(l1 == nullptr && l2 == nullptr)
-            return head;
-        else if(l1 == nullptr)
+            return nullptr;
+        else if(l1 == nullptr && l2 != nullptr)
             return l2;
-        else if(l2 == nullptr)
+        else if(l1 != nullptr && l2 == nullptr)
             return l1;
-        else{}
         
-        while(l1 != nullptr && l2 != nullptr){
-            if(head == nullptr){
-                if(l1->val < l2->val){
-                    head = new ListNode(l1->val);
-                    l1 = l1->next;
-                }
-                else if(l1->val > l2->val){
-                    head = new ListNode(l2->val);
-                    l2 = l2->next;
-                }
-                else{
-                    head = new ListNode(l1->val);
-                    head->next = new ListNode(l1->val);
-                    l1 = l1->next;
-                    l2 = l2->next;
-                }
-            }
-            else if(l1->val < l2->val){
-                ListNode* temp = new ListNode(l1->val);
-                ListNode* current = head;
-                current = lastNode(current);
-                current->next = temp;
-                l1 = l1->next;
-            }
-            else if(l1->val > l2->val){
-                ListNode* temp = new ListNode(l2->val);
-                ListNode* current = head;
-                current = lastNode(current);
-                current->next = temp;
-                l2 = l2->next;
-            }
-            else if(l1->val == l2->val){
-                ListNode* temp1 = new ListNode(l1->val);
-                ListNode* temp2 = new ListNode(l2->val);
-                ListNode* current = head;
-                current = lastNode(current);
-                current->next = temp1;
-                current->next->next = new ListNode(l2->val);
-                l1 = l1->next;
-                l2 = l2->next;
-            }
-            else{}
+        ListNode* ret = nullptr;
+        ListNode* temp = nullptr;
+        
+        if(l1->val > l2->val){
+            temp = new ListNode(l2->val);
+            l2 = l2->next;
+            ret = temp;
         }
-        
-        if(l1 == nullptr && l2 != nullptr){
-            ListNode* current = head;
-            current = lastNode(current);
-            current->next = l2;
+        else if(l1->val < l2->val){
+            temp = new ListNode(l1->val);
+            l1 = l1->next;
+            ret = temp;
         }
         else{
-            ListNode* current = head;
-            current = lastNode(current);
-            current->next = l1;
-        }        
-        return head;
+            temp = new ListNode(l1->val);
+            ret = temp;
+            temp->next = new ListNode(l1->val);
+            l1 = l1->next;
+            l2 = l2->next;
+            temp = temp->next;
+        }
+        
+        
+        while(l1 != nullptr && l2 != nullptr){
+            cout << l1->val << " " << l2->val << endl;
+            if(l1->val > l2->val){
+                temp->next = new ListNode(l2->val);
+                l2 = l2->next;
+            }
+            else if(l1->val < l2->val){
+                temp->next = new ListNode(l1->val);
+                l1 = l1->next;
+            }
+            else{
+                temp->next = new ListNode(l1->val);
+                temp = temp->next;
+                temp->next = new ListNode(l1->val);
+                l1 = l1->next;
+                l2 = l2->next;
+            }
+            temp = temp->next;
+        }
+        
+        if(l1 == nullptr){
+            while(l2 != nullptr){
+                temp->next = new ListNode(l2->val);
+                l2 = l2->next;
+                temp = temp->next;
+            }
+        }
+        else{
+            while(l1 != nullptr){
+                temp->next = new ListNode(l1->val);
+                l1 = l1->next;
+                temp = temp->next;
+            }
+        }
+        
+        return ret;
     }
 };
