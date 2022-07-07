000504cacc75968f95ab5c19716087df3c8ca8dd
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2022-07-06 20:05:36 -0400

    alien-dictionary solution

--- src/alien-dictionary.cpp
+++ src/alien-dictionary.cpp
@@ -1,54 +1,50 @@
 class Solution {
-public:
     map<char, set<char>> adj;
-    string reverse;
-    set<char> path;
-
+    set<char> visited;
+    string reverse = "";
+public:
     bool dfs(char c){
-        if(path.find(c) != path.end()){
+        if(visited.find(c) != visited.end()){
             return true;
         }
 
-        path.insert(c);
-        for(int i = 0; i < adj[c].size(); i++){
-            if(dfs(adj[c][i])){
+        visited.insert(c);
+        for(set<int>::iterator it = adj[c].begin(); it != adj[c].end(); it++){
+            if(dfs(*it)){
                 return true;
             }
         }
-        path.erase(c);
+        visited.erase(c);
         reverse.push_back(c);
         return false;
     }
 
     string alienOrder(vector<string> &words) {
-        map<char, set<char>> adj;
         for(int i = 0; i < words.size() - 1; i++){
-            string word1 = words[i];
-            string word2 = words[i+1];
-            int minLen = min(word1.length(), word2.length());
-            if(word1.length() > word2.length() && word1.substr(0, minLen) == word2.substr(0, minLen)){
+            string w1 = words[i];
+            string w2 = words[i+1];
+            int minLen = min(w1.length(), w2.length());
+            if(w1.length() > w2.length() && w1.substr(0, minLen) == w2.substr(0, minLen)){
                 return "";
             }
             for(int j = 0; j < minLen; j++){
-                if(word1[j] != word2[j]){
-                    adj[word1[j]].insert(word2[j]);
+                if(w1[j] != w2[j]){
+                    adj[w1[j]].push_back(w2[j]);
                     break;
                 }
             }
         }
 
-        for(auto const& c : adj){
-            if(dfs(c.first)){
+        for (std::map<char,set<char>>::iterator it=adj.begin(); it!=adj.end(); ++it){
+            if(dfs(*it)){
                 return "";
             }
         }
 
         string sol = "";
-
-        for(int i = reverse.length() - 1; i >= 0; i--){
+        for(int i = reverse.length() - 1; i != -1; i--){
             sol += reverse[i];
         }
-        return sol;
-
+        retur sol;
     }
 };
