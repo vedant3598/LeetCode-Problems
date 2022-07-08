0766bdd2ab1728c68e154efee6c7020c6611c802
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2022-07-08 13:37:41 -0400

    graph-valid-tree solution

--- src/graph-valid-tree.cpp
+++ src/graph-valid-tree.cpp
@@ -1,17 +1,18 @@
 class Solution {
+    map<int, vector<int>> adj;
+    set<int> visited;
 public:
     /**
      * @param n: An integer
      * @param edges: a list of undirected edges
      * @return: true if it's a valid tree, or false
      */
-    map<int, vector<int>> adj;
-    set<int> visited;
+
     bool dfs(int node, int prev){
         if(visited.find(node) != visited.end()){
             return false;
         }
-
+        
         visited.insert(node);
         for(int i = 0; i < adj[node].size(); i++){
             if(adj[node][i] != prev){
@@ -26,15 +27,13 @@ public:
     bool validTree(int n, vector<vector<int>> &edges) {
         for(int i = 0; i < edges.size(); i++){
             adj[edges[i][0]].push_back(edges[i][1]);
-            adj[edges[i][1]].push_back(edges[i][0]);
+            adj[edges[i][0]].push_back(edges[i][1]);
         }
 
-        if(!dfs(0, -1)){
+        if(!dfs(0, -1) || visited.size() != n){
             return false;
         }
 
-        if(visited.size() == n)
-            return true;
-        return false;
+        return true;
     }
 };
