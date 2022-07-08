f33e1542ac13d4ea6fda38850e7d477000e64e58
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2022-07-08 13:53:10 -0400

    number-of-connected-components-in-an-undirected-graph solution

--- src/number-of-connected-components-in-an-undirected-graph.cpp
+++ src/number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,28 +1,30 @@
-    set<int> visited;
+class Solution{
     map<int, vector<int>> adj;
-    
+    set<int> visited;
+public:
     void dfs(int node){
-        if(visited.find(node) != visited.end())
-            return;
-        
         visited.insert(node);
+        
         for(int i = 0; i < adj[node].size(); i++){
-            dfs(adj[node][i]);
+            if(visited.find(adj[node][i]) == visited.end()){
+                dfs(adj[node][i], node);
+            }
         }
     }
-    
-    int connectedComponents(int n, vector<vector<int>>& edges){
+
+    int connectedComponents(int n, vector<vector<int>> &edges){
+        int components = 0;
         for(int i = 0; i < edges.size(); i++){
             adj[edges[i][0]].push_back(edges[i][1]);
             adj[edges[i][1]].push_back(edges[i][0]);
         }
-        
-        int sol = 0;
+
         for(int i = 0; i < n; i++){
             if(visited.find(i) == visited.end()){
                 dfs(i);
-                sol++;
-            }            
+                components++;
+            }
         }
-        return sol;
+        return components;
     }
+};
