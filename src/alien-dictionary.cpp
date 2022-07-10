7d8dd3c2c0cc793debf765993547b372cfbbf957
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2022-07-10 12:29:25 -0400

    alien-dictionary solution

--- src/alien-dictionary.cpp
+++ src/alien-dictionary.cpp
@@ -1,30 +1,36 @@
 class Solution {
-    map<char, set<char>> adj;
+    unordered_map<char, vector<char>> adj;
     set<char> visited;
     string reverse = "";
 public:
     bool dfs(char c){
         if(visited.find(c) != visited.end()){
-            return true;
+            return false;
         }
 
         visited.insert(c);
-        for(set<int>::iterator it = adj[c].begin(); it != adj[c].end(); it++){
-            if(dfs(*it)){
-                return true;
+        for(int i = 0; i < adj[c].size(); i++){
+            if(!dfs(adj[c][i])){
+                return false;
             }
         }
         visited.erase(c);
         reverse.push_back(c);
-        return false;
+        return true;
     }
 
     string alienOrder(vector<string> &words) {
+        for(string word : words){
+            for(int i = 0; i < word.length(); i++){
+                adj[word[i]] = {};
+            }
+        }
+
         for(int i = 0; i < words.size() - 1; i++){
             string w1 = words[i];
             string w2 = words[i+1];
             int minLen = min(w1.length(), w2.length());
-            if(w1.length() > w2.length() && w1.substr(0, minLen) == w2.substr(0, minLen)){
+            if(w1.length() > w2.length && w1.substr(0, minLen) == w2.substr(0, minLen)){
                 return "";
             }
             for(int j = 0; j < minLen; j++){
@@ -33,18 +39,18 @@ public:
                     break;
                 }
             }
-        }
+        } 
 
-        for (std::map<char,set<char>>::iterator it=adj.begin(); it!=adj.end(); ++it){
-            if(dfs(*it)){
+        for(unordered_map<char, vector<char>>::iterator it = adj.begin(); it != adj.end(); it++){
+            if(!dfs(it->first)){
                 return "";
             }
-        }
+        }   
 
         string sol = "";
         for(int i = reverse.length() - 1; i != -1; i--){
             sol += reverse[i];
         }
-        retur sol;
+        return sol;
     }
 };
