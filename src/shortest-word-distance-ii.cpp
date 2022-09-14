68002262f893eb6c6381dff8c4f558841756aef7
Vedant Shah <36120106+vedant3598@users.noreply.github.com>
2022-09-14 08:29:35 -0400

    shortest-word-distance-ii solution

---
+++ src/shortest-word-distance-ii.cpp
@@ -0,0 +1,31 @@
+class ShortestDistance {
+    map<string, vector<int>> ind;
+public:
+    ShortestDistance(vector<string> &words){
+        for(int i = 0; i < words.size(); i++){
+            ind[words[i]].push_back(i);
+        }
+    }
+
+    int shortestDistance(string &word1, string &word2) {
+        for(int i = 0; i < words.size(); i++){
+            ind[words[i]].push_back(i);
+        }
+
+        vector<int> w1 = ind[word1];
+        vector<int> w2 = ind[word2];
+        int i1 = 0;
+        int i2 = 0;
+        int sol = words.size();
+        while(i1 < w1.size() && i2 < w2.size()){
+            sol = min(sol, abs(w2[i2]-w1[i1]));
+            if(i2 > i1){
+                i1++;
+            }
+            else{
+                i2++;
+            }
+        }
+        return sol;
+    }
+};
